8fb4a4dca1105ab105df861127c18f30205d9ca1
Dugduy <dugduy01@gmail.com>
2022-06-13 15:18:38 +0700

    fake cdn and edit a.js

--- cdn-faker.py
+++ cdn-faker.py
@@ -1,19 +1,29 @@
 from http.server import BaseHTTPRequestHandler,ThreadingHTTPServer
+from mimetypes import guess_type
+remixing={
+    'popsapp/assets/js/videojs-youtube.min.js':'a.js'
+}
 
 from requests import get, post
 class mybackend(BaseHTTPRequestHandler):
     def do_GET(self):
-        myheader=self.headers
-        myheader['host']='cdn.popsww.com'
-        res=get('https://cdn.popsww.com'+self.path,headers=myheader)
-        self.send_response(res.status_code)
-        # for k,v in res.headers.items():
-        #     self.send_header(k,v)
-        self.send_header('content-type',res.headers['content-type'])
-        # self.send_header('set-cookie',res.headers['set-cookie'])
-        self.end_headers()
-        self.wfile.write(res.content)
-        # print(res.content)
+        if self.path[1:] in remixing:
+            self.send_response(200)
+            self.send_header('content-type',guess_type(remixing[self.path[1:]])[0])
+            self.end_headers()
+            self.wfile.write(open(remixing[self.path[1:]],'rb').read())
+        else:
+            myheader=self.headers
+            myheader['host']='cdn.popsww.com'
+            res=get('https://cdn.popsww.com'+self.path,headers=myheader)
+            self.send_response(res.status_code)
+            # for k,v in res.headers.items():
+            #     self.send_header(k,v)
+            self.send_header('content-type',res.headers['content-type'])
+            # self.send_header('set-cookie',res.headers['set-cookie'])
+            self.end_headers()
+            self.wfile.write(res.content)
+            # print(res.content)
     def do_POST(self):
         self.headers['host']='cdn.popsww.com'
         res=post('https://cdn.popsww.com'+self.path,headers=self.headers)
