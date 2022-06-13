a02f65ebfed9abcb4740beb1c77c03019b712b46
Dugduy <dugduy01@gmail.com>
2022-06-13 11:27:35 +0700

    fake cdn

--- cdn-faker.py
+++ cdn-faker.py
@@ -1,22 +1,22 @@
-from http.server import BaseHTTPRequestHandler,HTTPServer
+from http.server import BaseHTTPRequestHandler,ThreadingHTTPServer
 
 from requests import get, post
 class mybackend(BaseHTTPRequestHandler):
     def do_GET(self):
         myheader=self.headers
-        myheader['host']='pops.vn'
-        res=get('http://pops.vn'+self.path,headers=myheader)
+        myheader['host']='cdn.popsww.com'
+        res=get('https://cdn.popsww.com'+self.path,headers=myheader)
         self.send_response(res.status_code)
         # for k,v in res.headers.items():
         #     self.send_header(k,v)
         self.send_header('content-type',res.headers['content-type'])
-        self.send_header('set-cookie',res.headers['set-cookie'])
+        # self.send_header('set-cookie',res.headers['set-cookie'])
         self.end_headers()
-        self.wfile.write(res.content.replace(b'https://cdn.popsww.com',b'http://localhost:8080'))
+        self.wfile.write(res.content)
         # print(res.content)
     def do_POST(self):
-        self.headers['host']='pops.vn'
-        res=post('http://pops.vn'+self.path,headers=self.headers)
+        self.headers['host']='cdn.popsww.com'
+        res=post('https://cdn.popsww.com'+self.path,headers=self.headers)
         self.send_response(res.status_code)
         # for k,v in res.headers.items():
         #     self.send_header(k,v)
@@ -24,6 +24,6 @@ class mybackend(BaseHTTPRequestHandler):
         self.send_header('set-cookie',res.headers['set-cookie'])
         self.end_headers()
         self.wfile.write(res.content)
-s=HTTPServer(('',80),mybackend)
+s=ThreadingHTTPServer(('',8080),mybackend)
 print('Started server!')
 s.serve_forever()
